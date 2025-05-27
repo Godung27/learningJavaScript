e02861aff4826d563bddf2980f1078f28904e078
Godung27 <parnuvich.l@gmail.com>
2025-05-27 20:40:13 +0700

    fundamentals javascript 2_Function Declarations and Expressions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -12,6 +12,7 @@ console.log(num);
 function sayMyName() {
   console.log("Godung");
 }
+// calling/ running/ invoking function
 sayMyName()
 sayMyName()
 sayMyName()
@@ -36,4 +37,55 @@ console.log(thailand, japan);
 
 console.log(describeCountry("Finland", 6, "Helsinki"));
 */
+
+
+/*
+                      // Function Declarations and Expressions
+  // Function Declarations
+function age1(birthyear) {
+  return 2025 - birthyear;
+}
+const godungAge1 = age1(2002);
+
+  // Function Expressions
+
+// const godungAge2 = age2(2002);
+// const age2 = function (birthyear) {
+//   return 2025 - birthyear;
+// }
+
+// *** can't define before function
+const age2 = function (birthyear) {
+  return 2025 - birthyear;
+}
+const godungAge2 = age2(2002);
+
+console.log(godungAge1, godungAge2);
+
+    // Quiz
+
+function percentageOfWorld1(population) {
+  return population / 7900 * 100;
+}
+const thaiPopPer1 = percentageOfWorld1(300);
+const japanPopPer1 = percentageOfWorld1(200);
+const finlandPopPer1 = percentageOfWorld1(600);
+
+console.log(`Thai ${thaiPopPer1}, Japan ${japanPopPer1}, Finland ${finlandPopPer1}`);
+
+const percentageOfWorld2 = function (population) {
+  return population / 7900 * 100;
+}
+const thaiPopPer2 = percentageOfWorld2(300);
+const japanPopPer2 = percentageOfWorld2(200);
+const finlandPopPer2 = percentageOfWorld2(600);
+
+console.log(`Thai ${thaiPopPer2}, Japan ${japanPopPer2}, Finland ${finlandPopPer2}`);
+*/
+
+
+
+
+
+
 
