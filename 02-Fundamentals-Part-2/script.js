c46da678eabf8eac3c2730139274b8a66cd6732b
Godung27 <parnuvich.l@gmail.com>
2025-05-27 19:11:17 +0700

    start fundamentals javascript 2_function

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,39 @@
+"use strict"
+/*
+// const num = 2;
+let num = 2;
+num += 1;
+console.log(num);
+*/
+
+
+/*
+                      // Functions
+function sayMyName() {
+  console.log("Godung");
+}
+sayMyName()
+sayMyName()
+sayMyName()
+
+function myKFC(chiken, coke) {
+  const comboKFC = `I buy combo KFC, it's got ${chiken} chiken and ${coke} coke`;
+  return comboKFC;
+}
+const myChiken = myKFC(0, 5);
+console.log(myChiken);
+
+const myCombo = myKFC(10, 6);
+console.log(myCombo);
+
+    // Quiz
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+const thailand = describeCountry("Thailand", 3, "Bangkok");
+const japan = describeCountry("Japan", 2, "Tokyo");
+console.log(thailand, japan);
+
+console.log(describeCountry("Finland", 6, "Helsinki"));
+*/
+
