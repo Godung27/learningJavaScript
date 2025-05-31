8c2f85119bf30096899941934a9dcc662d39f3bb
Godung27 <parnuvich.l@gmail.com>
2025-06-01 03:23:41 +0700

    fundamentals javascript 2_CHALLENGE #4

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -598,7 +598,7 @@ while (dice !== 1) {
 }
 // While loop just need condition
 
-// Quiz
+    // Quiz
 const populations = [300, 200, 600, 400];
 function percentageOfWorld1(population) {
   return population / 7900 * 100;
@@ -611,5 +611,35 @@ while (i < populations.length) {
 }
 console.log(percentages3);
 */
+
+
+/*
+        // CHALLENGE #4
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(bills[i] + tips[i]);
+}
+console.log(totals);
+
+// BONUS
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+  }
+  return sum / arr.length;
+}
+console.log(calcAverage(totals));
+*/
+
 
 
