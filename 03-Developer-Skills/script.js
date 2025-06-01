833e49015d57705617819e7d2a8267c7b95e5936
Godung27 <parnuvich.l@gmail.com>
2025-06-01 20:39:39 +0700

    developer skills_CHALLENGE #1

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -65,4 +65,80 @@ const amplitudeNew = calcTempAmplitudeNew(temperatures, [17, 0, -10]);
 console.log(amplitudeNew);
 */
 
+/*
+                      // Debugging with the Console and Breakpoints
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+
+    // C) FIX
+    value: Number(prompt('Degrees celsius:')),
+  };
+
+  // B) FIND
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY
+console.log(amplitudeBug);
+*/
+
+
+/*
+        // CHALLENGE #1
+// Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+// Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+// Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+// Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+// TEST DATA 1: [17, 21, 23]
+// TEST DATA 2: [12, 5, -5, 0, 4]
+
+const testTem1 = [17, 21, 23];
+const testTem2 = [12, 5, -5, 0, 4];
+const temperatures = testTem1.concat(testTem2);
+const godung = [];
+const printForecast = function (arr) {
+
+  for (let i = 0; i < temperatures.length; i++) {
+    godung.push(`... ${temperatures[i]}ºC in ${temperatures.indexOf(temperatures[i]) + 1} days`);
+  }
+  // godung.push("...");
+}
+printForecast(temperatures);
+console.log(godung.join("") + "...");
+*/
+
 
