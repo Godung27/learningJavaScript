a9f2592ea1e692fbaeff750917f19555be410adf
Godung27 <parnuvich.l@gmail.com>
2025-06-05 17:43:39 +0700

    Guess My Number_Handling Click Events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,4 +1,6 @@
 'use strict';
+/*
+                      // Selecting and Manipulating Elements
 console.log(document.querySelector(".between").textContent);
 console.log(document.querySelector(".message").textContent);
 document.querySelector(".message").textContent = "correct ! 🎉";
@@ -9,3 +11,16 @@ document.querySelector(".score").textContent = 18;
 
 document.querySelector(".guess").value = 10;
 console.log(document.querySelector(".guess").value);
+*/
+
+
+
+// Handling Click Events
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number!"
+  }
+})
