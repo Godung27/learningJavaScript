e49b734455c009dc5bc5614fb9906f5cb9247211
Godung27 <parnuvich.l@gmail.com>
2025-06-09 00:46:27 +0700

    Pig Game_Holding Current Score

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -19,26 +19,48 @@ const score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+}
+
 diceEl.classList.add("hidden");
 
 btnRoll.addEventListener("click", function () {
-  const diceNum = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    const diceNum = Math.trunc(Math.random() * 6) + 1;
 
-  diceEl.classList.remove("hidden");
-  diceEl.src = `dice-${diceNum}.png`;
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${diceNum}.png`;
 
-  if (diceNum !== 1) {
-    currentScore += diceNum;
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+    if (diceNum !== 1) {
+      currentScore += diceNum;
+      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else {
+      switchPlayer();
+    }
   }
 })
 
 btnHold.addEventListener("click", function () {
+  if (playing) {
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = score[activePlayer];
 
+    if (score[activePlayer] >= 10) {
+      playing = false;
+
+      document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
+      document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+
+      diceEl.classList.add("hidden");
+    } else {
+      switchPlayer();
+    }
+  }
 })
