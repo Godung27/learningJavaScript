30b4f3857fda3ef061218b3ab9f04e8e6f64d5b8
Godung27 <parnuvich.l@gmail.com>
2025-06-08 23:09:17 +0700

    Pig Game_Switching the Active Player

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Pig Game
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const currentS0 = document.getElementById("current--0");
@@ -12,7 +14,10 @@ const btnHold = document.querySelector(".btn--hold");
 
 score0El.textContent = 0;
 score1El.textContent = 0;
+
+const score = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 diceEl.classList.add("hidden");
 
@@ -24,10 +29,16 @@ btnRoll.addEventListener("click", function () {
 
   if (diceNum !== 1) {
     currentScore += diceNum;
-    currentS0.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 })
 
+btnHold.addEventListener("click", function () {
 
+})
