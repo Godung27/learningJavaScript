20bdcfdeea2a7030a014d145335ff5aacb8d84a9
Godung27 <parnuvich.l@gmail.com>
2025-06-08 17:07:55 +0700

    Modal Window_Working With Classes

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -2,12 +2,24 @@
 
 // Modal Window
 const modal = document.querySelector(".modal");
-const Overlay = document.querySelector(".overlay");
+const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
 
-console.log(btnsShowModal);
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
 
 for (let i = 0; i < btnsShowModal.length; i++) {
-  console.log(btnsShowModal[i].textContent);
+  btnsShowModal[i].addEventListener("click", openModal);
 }
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
