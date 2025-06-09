0c77429a9123bf8509d74e7d5b94f8cc24cf4600
Godung27 <parnuvich.l@gmail.com>
2025-06-09 17:38:45 +0700

    Behind the Scenes_Hoisting and TDZ in Practice

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,6 +1,6 @@
 'use strict';
-
-// Scoping in Practice
+/*
+                      // Scoping in Practice
 const firstName = "Godung";
 
 function me(birthYear) {
@@ -26,3 +26,47 @@ function me(birthYear) {
   godungJob();
 }
 me(2002);
+*/
+
+
+/*
+                      // Hoisting and TDZ in Practice
+// Varibles
+console.log(firstName); // undefined
+// console.log(lastName); // TDZ (Temporal Dead Zone)
+// console.log(year); // TDZ
+
+var firstName = "Godung";
+const lastName = "Lim";
+let year = 2002;
+
+// Function
+console.log(funDecla(2, 7)); // 9
+// console.log(funExpr(3, 1)); // TDZ
+// console.log(funArrow(5, 5)); // TDZ
+
+function funDecla(a, b) {
+  return a + b;
+}
+
+const funExpr = function (a, b) {
+  return a + b;
+}
+
+var funArrow = (a, b) => a + b;
+
+    // Example
+console.log(potato); // undefined
+
+if (!potato) {
+  console.log(deleteProduct()); // All products delete!
+}
+
+var potato = 5
+
+function deleteProduct() {
+  console.log("All products delete!")
+}
+*/
+
+
