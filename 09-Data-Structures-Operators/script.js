0a96a83530487db30aaceeafacb4ad99da475314
Godung27 <parnuvich.l@gmail.com>
2025-06-13 01:43:58 +0700

    Data Structures-Operators_The Nullish Coalescing Operator (??)

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -247,6 +247,19 @@ restaurant.orderPasta && restaurant.orderPasta("Mushroom", "Chiken", "Pepper");
 */
 
 
+/*
+                      // The Nullish Coalescing Operator (??)
+// Nullish: null and undefined (Not 0 or " ") 
+  // Nullish (first True)
+restaurant.numGuests = 0;
+const guestZero = restaurant.numGuests ?? 10;
+console.log(guestZero); // 0
+
+restaurant.numGuests = undefined;
+const unGuest = restaurant.numGuests ?? 10;
+console.log(unGuest); // 10
+*/
+
 
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
