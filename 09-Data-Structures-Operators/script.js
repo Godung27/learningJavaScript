3f5720f960f6b71fa85c7d3b55fbc9da5b65b945
Godung27 <parnuvich.l@gmail.com>
2025-06-15 00:29:38 +0700

    Data Structures-Operators_Enhanced Object Literals

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -306,6 +306,50 @@ for (const [i, el] of menu.entries()) {
 */
 
 
+/*
+                      // Enhanced Object Literals
+const weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+const openingHours = {
+  // ES6
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[4 + 2] {
+    open: 0,
+    close: 24,
+  }
+};
+
+const restaurantES6 = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // openingHours: openingHours // Old one
+  // ES6 Enhanced Object Literals
+  openingHours,
+
+  // Function ES6
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+};
+console.log(restaurantES6);
+*/
+
+
 
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
