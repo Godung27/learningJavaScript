87c52bff89797eeb0e965a33948a102f08d543e1
Godung27 <parnuvich.l@gmail.com>
2025-06-20 02:56:50 +0700

    Data Structures-Operators_String Methods Practice

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -721,6 +721,20 @@ planesInLine(4); // There are 4 planes in line ✈️✈️✈️✈️
 planesInLine(7)
 */
 
-
+/*
+                      // String Methods Practice
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const sliceStr = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split("+")) {
+  const [type, form, to, time] = flight.split(";");
+  const str = `${type.startsWith("_Delayed") ? "🔴" : ""}${type.replaceAll("_", " ")} form ${sliceStr(form)} to ${sliceStr(to)} (${time.replace(":", "h")})`.padStart(45);
+  console.log(str);
+}
+*/
