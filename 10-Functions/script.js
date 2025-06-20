4b11e189cf15a9ebabaa45d5c4cd7397f000cd88
Godung27 <parnuvich.l@gmail.com>
2025-06-21 00:01:53 +0700

    Functions_Functions Accepting Callback Functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -50,9 +50,46 @@ const newPassport = function (person) {
   person.passport = Math.trunc(Math.random() * 10000000);
 }
 
-newPassport(godung);
+newPassport(godung); // (Heap)
 checkIn(flight, godung); // Wrong passport
 */
 
+
+/*
+                      // Functions Accepting Callback Functions
+const oneWord = function (str) {
+  return str.replaceAll(" ", "").toLowerCase();
+}
+
+const UpperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+}
+
+  // Higher-order function
+const transformStr = function (str, callFunction) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transform string: ${callFunction(str)}`);
+  console.log(`Function name: ${callFunction.name}`);
+}
+
+transformStr("Hello my name is Godung", oneWord);
+// Original string: Hello my name is Godung
+// Transform string: hellomynameisgodung
+// Function name: oneWord
+
+transformStr("Hello my name is Godung", UpperFirstWord);
+// Original string: Hello my name is Godung
+// Transform string: HELLO my name is Godung
+// Function name: UpperFirstWord
+
+  // JS use callbacks all the time
+const run = function () {
+  console.log("🏃‍♂️");
+}
+document.body.addEventListener("click", run);
+
+["Godung", "Smith"].forEach(run); // 🏃‍♂️🏃‍♂️
+*/
 
 
