680121ec0dc69dac429b55a134abe6dfd7064220
Godung27 <parnuvich.l@gmail.com>
2025-06-29 23:12:18 +0700

    Arrays Bankist_Array Methods Practice

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -448,7 +448,7 @@ console.log(randomNum);
 
 /*
                       // Non-Destructive Alternatives: toReversed, toSorted, toSpliced, with
-  // toReversed, toSorted, toSpliced, with: 
+  // toReversed, toSorted, toSpliced, with:
     // Create New Array
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -467,3 +467,66 @@ console.log(newMovements);
 console.log(movements);
 */
 
+
+/*
+                      // Array Methods Practice
+const accou1 = {
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+};
+const accou2 = {
+  movements: [5000, 3400, -150, -790, -3210],
+};
+const accou3 = {
+  movements: [200, -200, 340],
+};
+const accou4 = {
+  movements: [430, 1000],
+};
+const accous = [accou1, accou2, accou3, accou4];
+
+// 1.
+const depositSum = accous.flatMap(acc => acc.movements).filter(mov => mov > 0).reduce((sum, mov) => sum + mov, 0);
+
+console.log(depositSum);
+
+// 2.
+const numDeposit1000 = accous.flatMap(acc => acc.movements).reduce((count, cur) =>
+  cur >= 1000 ? ++count : count, 0);
+
+console.log(numDeposit1000);
+// Prefixed ++ operator
+let a = 10;
+// console.log(a++); // 10 (present pre value)
+// console.log(a); // 11
+console.log(++a); // 11
+console.log(a); // 11
+
+// 3.
+const { deposits, withdrawals } = accous.flatMap(acc => acc.movements).reduce((sums, cur) => {
+  // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+  sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+  return sums;
+}, { deposits: 0, withdrawals: 0 },);
+
+console.log(deposits, withdrawals);
+
+// 4.
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+    .join(' ');
+
+  return capitalize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+*/
